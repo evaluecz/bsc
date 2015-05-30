33a646ce18e9c931463142e812969e9b6334b74d
Josef Podany <podany.pepa@gmail.com>
2015-05-30 13:28:24 +0200

    1. funkcni kostra NodeJS aplikace

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+// Cislo portu, na kterem pobezi server
+var port = 9000;
+
+app.get('/', function (req, res) {
+    res.send('1. funkcni kostra aplikace...');
+});
+
+var server = app.listen(port, function () {
+
+    var host = server.address().address;
+    var port = server.address().port;
+    
+    console.log('Aplikace bezi na http://%s:%s', host, port);
+
+});
