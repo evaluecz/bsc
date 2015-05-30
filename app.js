10d2233e8ea192c91cc7553967e2938693973042
Josef Podany <podany.pepa@gmail.com>
2015-05-30 15:24:45 +0200

    drobne upravy

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var app = express();
 // Cislo portu, na kterem pobezi server
 var port = 9000;
 
+app.set('views', './views');
+
+
 app.get('/', function (req, res) {
     res.send('1. funkcni kostra aplikace...');
 });
