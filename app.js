a6346beb35990e3fff70930fb5d645dee81d1fa0
Josef Podany <podany.pepa@gmail.com>
2015-06-01 10:47:31 +0200

    views

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 var express = require('express');
 var app = express();
-
+ 
 // Cislo portu, na kterem pobezi server
 var port = 9000;
 
-app.set('views', './views');
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
 
-app.get('/', function (req, res) {
-    res.send('1. funkcni kostra aplikace...');
+// index page 
+app.get('/', function(req, res) {
+    res.render('pages/index');
 });
 
 var server = app.listen(port, function () {
